55683c2b73f23936dc9c97589ddb2961f84ae487
Gareth O'Sullivan <gareth.osullivan@icloud.com>
2017-08-16 10:37:19 +0100

    Music List segue setup

---
+++ SwappingScreens/MusicListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MusicListViewController.swift
+//  SwappingScreens
+//
+//  Created by Gareth O'Sullivan on 16/08/2017.
+//  Copyright © 2017 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class MusicListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
